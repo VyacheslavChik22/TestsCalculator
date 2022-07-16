061be1d3f6d04adb298410ce444fd939f3caae4a
Вячеслав <asyofka@mail.ru>
2022-07-16 16:17:02 +0300

    First commit

---
+++ src/main/java/com/vyacheslav/testscalculator/TestsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.vyacheslav.testscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vyacheslav/testscalculator/TestsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyacheslav.testscalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
